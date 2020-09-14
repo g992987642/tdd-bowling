1e2078699e4dd7cfd9217d75e236badfb8556566
g992987642 <992987642@qq.com>
2020-09-14 23:43:02 +0800

    [guhao]:impelment throw WrongPointsArraySizeException when false points array size is given

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,9 +1,10 @@
 import exception.WrongKnockPointException;
+import exception.WrongPointsArraySizeException;
 
 public class BowlingGame {
     int score=0;
     int turns=1;
-
+    int lastIndex=0;
     public int caculateScore(int[] knockdownPoints) {
         for(int i=0;i<knockdownPoints.length;){
             if(knockdownPoints[i]==10){
@@ -27,22 +28,29 @@ public class BowlingGame {
                 }
             }
         }
+
+        if(lastIndex!=knockdownPoints.length-1){
+            throw new WrongPointsArraySizeException();
+        }
         return score;
     }
     public void addScoreWhenFirstKnockdownPointIs10(int[]knockdownPoints,int index){
         score+=knockdownPoints[index];
         score+=knockdownPoints[index+1];
         score+=knockdownPoints[index+2];
+        lastIndex=index+2;
     }
 
     public void addScoreWhenFirstKnockdownPointIsNot10(int[]knockdownPoints,int index){
         int curKnockdownPoints=knockdownPoints[index];
         score+=curKnockdownPoints;
         index++;
+        lastIndex=index;
         int nextKnockdownPoints=knockdownPoints[index];
         score+=nextKnockdownPoints;
         if(curKnockdownPoints+nextKnockdownPoints==10){
             score+=knockdownPoints[index+1];
+            lastIndex=index+1;
         }
         if(curKnockdownPoints+nextKnockdownPoints>10){
             throw new WrongKnockPointException();
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,28 +1,39 @@
 import exception.WrongKnockPointException;
+import exception.WrongPointsArraySizeException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
     @Test
-    void should_return_score_correctly_when_right_knockdown_points_is_given_and_the_10th_turn_point_is_10()  {
-        BowlingGame bowlingGame=new BowlingGame();
-        int knockdownPoints[]={ 10,1,2,10,10,10,10,10,10,10,10,10,10};
-        int score=bowlingGame.caculateScore(knockdownPoints);
-        Assertions.assertEquals(256,score);
+    void should_return_score_correctly_when_right_knockdown_points_is_given_and_the_10th_turn_point_is_10() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int knockdownPoints[] = {10, 1, 2, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        int score = bowlingGame.caculateScore(knockdownPoints);
+        Assertions.assertEquals(256, score);
     }
 
     @Test
-    void should_return_score_correctly_when_right_knockdown_points_is_given_and_the_10th_turn_point_is_not_10(){
-        BowlingGame bowlingGame=new BowlingGame();
-        int knockdownPoints[]={ 10,1,2,10,10,10,10,10,10,10,2,3};
-        int score=bowlingGame.caculateScore(knockdownPoints);
-        Assertions.assertEquals(208,score);
+    void should_return_score_correctly_when_right_knockdown_points_is_given_and_the_10th_turn_point_is_not_10() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int knockdownPoints[] = {10, 1, 2, 10, 10, 10, 10, 10, 10, 10, 2, 3};
+        int score = bowlingGame.caculateScore(knockdownPoints);
+        Assertions.assertEquals(208, score);
     }
+
     @Test
-    void  should_throw_WrongKnockPointException_when_false_knockdown_points_is_given(){
-        BowlingGame bowlingGame=new BowlingGame();
-        int knockdownPoints[]={ 10,1,11,10,10,10,10,10,10,10,2,3};
-        Assertions.assertThrows(WrongKnockPointException.class,()->{
+    void should_throw_WrongKnockPointException_when_false_knockdown_points_is_given() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int knockdownPoints[] = {10, 1, 11, 10, 10, 10, 10, 10, 10, 10, 2, 3};
+        Assertions.assertThrows(WrongKnockPointException.class, () -> {
+            bowlingGame.caculateScore(knockdownPoints);
+        });
+    }
+
+    @Test
+    void should_throw_WrongPointsArraySizeException_when_false__points_Array_Size_is_given() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int knockdownPoints[] = {10, 1, 1, 10, 10, 10, 10, 10, 10, 10, 2, 3, 1};
+        Assertions.assertThrows(WrongPointsArraySizeException.class, () -> {
             bowlingGame.caculateScore(knockdownPoints);
         });
     }
