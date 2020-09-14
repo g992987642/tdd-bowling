839f67683a381a6d64805d9face6018ec9d740fc
g992987642 <992987642@qq.com>
2020-09-14 23:23:50 +0800

    [guhao]:return score correctly when right knockdown points is given and the 10th turn point is not 10

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -3,13 +3,19 @@ import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
     @Test
-    void should_return_score_correctly_when_right_knockdown_points_is_givenand_and_the_10th_turn_point_is_10()  {
+    void should_return_score_correctly_when_right_knockdown_points_is_given_and_and_the_10th_turn_point_is_10()  {
         BowlingGame bowlingGame=new BowlingGame();
         int knockdownPoints[]={ 10,1,2,10,10,10,10,10,10,10,10,10,10};
         int score=bowlingGame.caculateScore(knockdownPoints);
         Assertions.assertEquals(256,score);
     }
 
-
+    @Test
+    void should_return_score_correctly_when_right_knockdown_points_is_given_and_and_the_10th_turn_point_is_not_10(){
+        BowlingGame bowlingGame=new BowlingGame();
+        int knockdownPoints[]={ 10,1,2,10,10,10,10,10,10,10,2,3};
+        int score=bowlingGame.caculateScore(knockdownPoints);
+        Assertions.assertEquals(208,score);
+    }
 
 }
